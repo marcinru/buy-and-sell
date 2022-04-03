8d95aa6789a605bc382d7ebad2293a2932d5f8bc
Marcin Rudzki <mar.rudzki@gmail.com>
2022-04-03 15:29:10 +0200

    add components and routes for all pages

---
+++ src/app/listing-details-page/listing-details-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listing-details-page',
+  templateUrl: './listing-details-page.component.html',
+  styleUrls: ['./listing-details-page.component.css']
+})
+export class ListingDetailsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
