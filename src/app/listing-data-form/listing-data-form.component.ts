137d1cd0f8d8e6ff849a052de92a1cc15a109636
Marcin Rudzki <mar.rudzki@gmail.com>
2022-04-09 09:07:01 +0200

    add input buttonText

--- src/app/listing-data-form/listing-data-form.component.ts
+++ src/app/listing-data-form/listing-data-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,6 +7,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./listing-data-form.component.css']
 })
 export class ListingDataFormComponent implements OnInit {
+  @Input() buttonText: string = '';
   name: string = '';
   description: string = '';
   price: string = '';
